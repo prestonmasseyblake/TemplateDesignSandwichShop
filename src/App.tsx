392f45ce43f4d994d76b304d705adb4dd5f1aed8
Preston Massey-Blake <pmassey-blake@mail.csuchico.edu>
2023-05-06 18:39:00 -0700

    added more stuff

--- src/App.tsx
+++ src/App.tsx
@@ -21,21 +21,35 @@ const App: React.FC = () => {
   return (
     <div className="App">
       {/* hero section   */}
+       <h1 className="text-black font-bold text-center text-3xl mt-8">Template Method Sandwich Shop</h1>
       <div className="hero-section p-8">
-
-        <h1 className="text-black font-bold text-center text-3xl">Template Method Sandwich Shop</h1>
+          <div className="my-div">
+      {/* Your content here */}
+    </div>
+       
         <p className="text-black font-bold text-center py-8 text-2xl">Every Sandwich Comes With A Side Of Inheritance</p>
       </div>
       {/* hero section   */}
       {/* food section   */}
-      <div className="flex">
-        <div className="card m-4">
+      <div>
+       
+        </div>
+      <div className="w-full flex items-center justify-center">
+
+        <div className="card m-4 my-shadow p-4 bg-[#FFF]">
+          <div className="w-full  flex justify-center">
+          <img className="w-64 bg[#000]" src={process.env.PUBLIC_URL + "/ham_sandwich.png"} />
+          </div>
           <p>Ham Sandwich</p>
           <p>$10.99</p>
        <button className="bg-[#999] p-2 rounded-md" onClick={handleHamSandwichClick}>Prepare Ham Sandwich</button>
-      </div>
-        <div className="card m-4">
-          <p>Veggie Sandwich</p>
+        </div>
+        
+        <div className="card m-4 my-shadow p-4 bg-[#FFF]">
+          <div className="w-full  flex justify-center">
+          <img className="w-64 bg[#000]" src={process.env.PUBLIC_URL + "/veggie_sandwich.png"} />
+          </div>
+          <p className="">Veggie Sandwich</p>
           <p>$10.99</p>
         <button className="bg-[#999] p-2 rounded-md" onClick={handleVeggieSandwichClick}>Prepare Veggie Sandwich</button>
       </div>
@@ -43,6 +57,9 @@ const App: React.FC = () => {
         {/* food section   */}
 
       {/* display section     */}
+      <div className="w-full flex justify-center">
+      <div className="order-machine h-32  bg-[#222] w-3/6">
+        <p className="text-white">Order Screen</p>
        {sandwich && (
           <div className="">
             <h2>Preparing a {sandwich.constructor.name}:</h2>
@@ -53,9 +70,22 @@ const App: React.FC = () => {
             </ul>
           </div>
         )}
-        {/* display section     */}
-       
+        </div>
+        </div>
+      {/* display section     */}
       
+      {/* footer  */}
+      <div className="flex justify-between mt-8">
+        <div className="p-4">
+        <p className=" text-3xl italic">Built by Preston Blake & Manohar Mendan</p>
+        <p className=" text-2xl italic">CSCI 630 Software Design & Maintenance</p>
+        </div>
+        <div>
+        <img className="h-64" src={process.env.PUBLIC_URL + "/robot.png"} />
+        </div>
+
+       </div>
+        {/* footer  */}
     </div>
   );
 };
